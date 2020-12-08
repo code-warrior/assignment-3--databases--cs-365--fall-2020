9bca5baae6cfd01ff940c4994e4aef8169685f33
skearns19 <48564861+skearns19@users.noreply.github.com>
2020-12-08 17:24:27 -0500

    work with commands in JavaScript

---
+++ commands.js
@@ -0,0 +1,8 @@
+{"db.thePasswordsCollection.insert({\"user11\":{\n\"user\":{\n\"name\":\n{\"first\":\"Isaiah\",\n\"last\":\"Johnson\"\n},\n\"email\":\"IsaiahJ3@gmail.com\"\n},\n\"login_information\":{\n\"username\":\"IJohn346\",\n\"websiteName\":\"ESPN\",\n\"URL\":\"https://www.espn.com\",\n\"comment\": \"\"\n},\n\"password\":{\n\"passwordForWebsite\":\"IsaiahSports10\",\n\"passwordcreated\":\"Jul2016\"\n}\n}})",
+    "db.thePasswordsCollection.find({'user3.password.passwordForWebsite':'I$a1ah45'},{'user3.password.passwordForWebsite':1, '_id':0}).pretty()",
+    "db.thePasswordsCollection.find({'user1.login_information.URL':'https://www.google.com'},{'user1.password.passwordForWebsite':1,'user1.login_information.websiteName':1, 'user1.login_information.comment':1, 'user1.login_information.username':1,'user1.login_information.URL':1,'user1.password.passwordcreated':1,'_id':0}).pretty()",
+    "db.thePasswordsCollection.update({'user5.password.passwordForWebsite':'19793674'},{'$set':{'user5.login_information.URL':'https://www.microsoft.com'}})",
+    "db.thePasswordsCollection.update({'user7.password.passwordForWebsite':'Isaiah$g1thub'},{'$set':{'user7.password.passwordForWebsite':'newpassword'}})",
+    "db.thePasswordsCollection.update({'user9.password.passwordcreated':'Oct2017'},{$unset:{'user9.password.passwordForWebsite':'Ch@tisa1ah'}})"
+    "const db = db.getSiblingDB('passwords')"
+}
