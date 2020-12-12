7cf90e71708a6d5b0b292cdba25d889d0fc3087f
skearns19 <48564861+skearns19@users.noreply.github.com>
2020-12-12 12:44:02 -0500

    work with commands in JavaScript and working with database

--- commands.js
+++ commands.js
@@ -1,9 +1,10 @@
 const db = "passwords";
 const database = db.getSiblingDB(db);
 myObjectId = ObjectId("5fd2503f8b8bef95c4eaafef");
+database.thePasswordsCollection.update({"password.passwordcreated":ObjectId(myObjectId).getTimestamp()});
 database.thePasswordsCollection.insert({"_id":ObjectId("5fd2503f8b8bef95c4eaaff9"),"login_information":{"username":"IJohn346","websiteName":"ESPN","URL":"https://www.espn.com", "comment": ""},"password":{"passwordForWebsite":"IsaiahSports10","passwordcreated":ObjectId(myObjectId).getTimestamp()}});
-database.thePasswordsCollection.find({"user3.password.passwordForWebsite": "I$a1ah45"}, {"user3.password.passwordForWebsite": 1,"_id": 0}).pretty();
-database.thePasswordsCollection.find({"user1.login_information.URL": "https://www.google.com"}, {"user1.password.passwordForWebsite": 1, "user1.login_information.websiteName": 1, "user1.login_information.comment": 1, "user1.login_information.username": 1, "user1.login_information.URL": 1, "user1.password.passwordcreated": 1, "_id": 0}).pretty();
-database.thePasswordsCollection.update({"user5.password.passwordForWebsite": "19793674"}, {"$set": {"user5.login_information.URL": "https://www.microsoft.com"}});
-database.thePasswordsCollection.update({"user7.password.passwordForWebsite": "Isaiah$g1thub"}, {"$set": {"user7.password.passwordForWebsite": "newpassword"}});
-database.thePasswordsCollection.update({"user9.password.passwordcreated": "Oct2017"}, {"$unset": {"user9.password.passwordForWebsite": "Ch@tisa1ah"}});
+database.thePasswordsCollection.find({"password.passwordForWebsite": "I$a1ah45"}, {"password.passwordForWebsite": 1,"_id": 0}).pretty();
+database.thePasswordsCollection.find({"login_information.URL": "https://www.google.com"}, {"password.passwordForWebsite": 1, "login_information.websiteName": 1, "login_information.comment": 1, "login_information.username": 1, "login_information.URL": 1, "password.passwordcreated": 1, "_id": 0}).pretty();
+database.thePasswordsCollection.update({"password.passwordForWebsite": "19793674"}, {"$set": {"login_information.URL": "https://www.microsoft.com"}});
+database.thePasswordsCollection.update({"password.passwordForWebsite": "Isaiah$g1thub"}, {"$set": {"password.passwordForWebsite": "newpassword"}});
+database.thePasswordsCollection.update({"password.passwordcreated": "Oct2017"}, {"$unset": {"password.passwordForWebsite": "Ch@tisa1ah"}});
